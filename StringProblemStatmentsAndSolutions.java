3cccbcff1e08fd2a84bfca0abca5355b0b89c28e
aman-ipec2020 <59360163+aman-ipec2020@users.noreply.github.com>
2020-10-21 09:11:28 +0530

    String Problem Statement-2
    Write a program to Print duplicate characters from a String

--- StringProblemStatmentsAndSolutions.java
+++ StringProblemStatmentsAndSolutions.java
@@ -2,14 +2,42 @@ public class StringProblemStatementsAndSolutions
 {
     public static void main(String []args)
     {
+/*
 //      Question-1 : Java Program to Reverse a String in Place
         String input = "Kanupriya";
         System.out.println("Input : " + input);
         
         String output = inPlaceStringReverse(input);
         System.out.println("Output : " + output);
+*/
+
+//      Question-2 : Java Program to Print duplicate characters from a String
+        String input = "How many duplicates are there?";
+        System.out.println("Input : " + input);
+        
+        String output = printDuplicateCharactersFromString(input);
+        System.out.println("Output : " + output);
     }
      
+    public static String printDuplicateCharactersFromString(final String input)
+    {
+        int length = input.length();
+        
+        String characters = "";
+        String duplicates = "";
+
+        for(int i = 0; i<length-1; i++)
+        {
+            String current = Character.toString(input.charAt(i));
+            if(characters.contains(current))
+                if(!duplicates.contains(current))
+                    duplicates += current + ","; 
+            
+           characters += current;
+        }
+        return duplicates;
+    }
+
     public static String inPlaceStringReverse(final String input)
     {
         int length = input.length();
